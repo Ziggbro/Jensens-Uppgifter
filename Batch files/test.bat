863025f05446b20115208c2c1e79ef7abcc473f7
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-04-06 00:53:34 +0200

    Create test.bat

---
+++ Batch files/test.bat
@@ -0,0 +1,21 @@
+
+TITLE - ProvUpgifft
+
+md C:\Users\Default\Desktop\ Mapp
+
+cd C:\Users\Default\Desktop\Mapp
+
+ echo "hello">Mapp\new.html
+
+
+
+"@echo off
+cd %USERPROFILE%\Desktop
+md MyFolder
+cd MyFolder
+md Subfolder1
+md Subfolder2
+md Subfolder3
+echo Hello, this is a file. > Subfolder1\File1.txt
+echo Hello, this is a file. > Subfolder2\File2.txt
+echo Hello, this is a file. > Subfolder3\File3.txt"
