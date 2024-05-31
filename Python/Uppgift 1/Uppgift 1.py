094fef3dbeec99771a4df1301694de3ec9ab39c9
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-05-31 14:26:18 +0200

    printar en massa number

---
+++ Python/Uppgift 1/Uppgift 1.py
@@ -0,0 +1,8 @@
+print("languages:\n\tPython\n\tJava\n\tJavaScript")
+
+print("12 - 7 \n=")
+print(12-7)
+print("24 / 3 + 9 \n=")
+print(24/3+9)
+print("8 * 2 (5 - 3) + 16 / 4 - 1 \n=")
+print(8*2 * (5 -3)+16/4 - 1)
