b0e258612c3abe0e824c646a168568bb8ce5cae7
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-07 15:31:54 +0200

    Create vikt koll.py

---
+++ Python/Uppgift 5/vikt koll.py
@@ -0,0 +1,8 @@
+
+vikt = float(input("vikt i kg: "))
+if vikt < 20:
+    print("500kg knark till barnet.")
+elif 20 <= vikt <= 40:
+    print("750kg knark till snubben")
+elif 41 <= vikt <= 60:
+    print("1000kg knark till gubben")
