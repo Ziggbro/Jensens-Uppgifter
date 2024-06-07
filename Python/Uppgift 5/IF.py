15f8977449fdd16ccab63a3f4cddeaa624d547d8
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-07 15:00:44 +0200

    drugs for kids

--- Python/Uppgift 5/IF.py
+++ Python/Uppgift 5/IF.py
@@ -1,4 +1,11 @@
 
+svar = input("är patienten barn? Y/N")
+
+if svar == "Y":
+    print("Patienten behöver bara 500mg per dag.")
+
+if svar == "N":
+    print("Patienten behöver 700mg per dag.")
 
 
 
