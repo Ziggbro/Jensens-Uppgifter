7ee9ba39adb3c821331fab5490ee327d219198d9
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-12 00:21:32 +0200

    övning

---
+++ Python/Uppgift 4/Uppgift 4.py
@@ -0,0 +1,11 @@
+
+namn = input ("Skrin ditt namn: ")
+print("Hej " namn)
+
+ålder = int(input("skriv din ålder:"))
+print("Om fem år är du :", ålder + 5, "år.")
+
+längd= float(input("Skriv din längd i meter: "))
+print("du är alltså ", längd, * 100, "cm.")
+
+print("stämmer det?")
