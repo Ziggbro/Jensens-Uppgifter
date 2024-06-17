472ad2fdf39ce0b45d2b63124cff2a709dafbd6a
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-17 22:05:32 +0200

    sten sax pose

--- Python/Inlämnings uppgifter/Inlämning.py
+++ Python/Inlämnings uppgifter/Inlämning.py
@@ -7,4 +7,19 @@ måste innehålla
 -If-satser
 -Functions (Alernativ)
 """
+import random
 
+
+n=1
+val = ("sten","sax","påse")
+spelare = None
+dator = random.choice(val)
+
+
+print("Let's play a game")
+#print("Please pick wich one.")
+#wichgame = int(print("1:   sten saxs påse \n 2: Tärning \n 3: Something else"))
+while spelare not in val:
+    spelare = input("välj sten sax eller påse")
+    
+    
