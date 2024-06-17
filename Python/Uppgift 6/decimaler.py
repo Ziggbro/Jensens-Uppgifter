f6f6941e6b82e9163c77cf25671fabbc3097a3a5
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-17 21:33:27 +0200

    Update decimaler.py

--- Python/Uppgift 6/decimaler.py
+++ Python/Uppgift 6/decimaler.py
@@ -3,7 +3,7 @@ tal = float(input("skriv ett tal"))
 
 if tal > 40:
     print("talet e storre en 40")
-elif tall == 40:
+elif tal == 40:
     print("talet e 40")
 else:
     print("talet e inte mer en 40")
