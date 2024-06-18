0f9dfc6108ea774bb361eb9d1dcd951949a7fe3f
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-18 03:39:45 +0200

    Created a test copy to play with

---
+++ Python/Inlämnings uppgifter/Inlämning copy.py
@@ -0,0 +1,48 @@
+"""""
+måste innehålla
+-Variabler
+-Input-kommando
+-En eller flera print-satser
+-for- eller while-slingor
+-If-satser
+-Functions (Alernativ)
+"""
+import random
+
+
+
+val = ("sten","sax","påse")
+spelare = None
+dator = random.choice(val)
+
+
+print("Let's play a game")
+#print("Please pick wich one.")
+#wichgame = int(print("1:   sten saxs påse \n 2: Tärning \n 3: Something else"))
+def gameStart():
+    if input("1"):
+        print("du har valt sten sax påse")
+        tresome()
+    else:
+        print("game machine broken, pick another")
+        
+
+def tresome():
+    while spelare not in val:
+        spelare = input("välj sten sax eller påse")
+    else :
+        print("välj ett riktigt val")
+
+    print("spelare valde :", spelare)
+    print("Datorn valde:", dator)
+    if spelare == dator:
+        print("Ovavgjort")
+    elif spelare == "sax" and dator == "påse":
+        print("du vinner")
+    elif spelare == "påse" and dator == "sten":
+        print("du vinner")
+    elif spelare == "sten" and dator == "sax":
+        print("du vinner")
+    else:
+        print("Du förlorade")
+    print("vill du spela igen?")
