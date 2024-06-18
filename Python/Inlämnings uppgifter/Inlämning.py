6e7ebfb417667167006279c132452a8b111f5195
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-18 22:09:26 +0200

    Update Inlämning.py

--- Python/Inlämnings uppgifter/Inlämning.py
+++ Python/Inlämnings uppgifter/Inlämning.py
@@ -9,7 +9,7 @@ måste innehålla
 """
 import random
 
-options = ("rock", "paper", "scissors")
+options = ("sten", "sax", "påse")
 running = True
 
 while running:
@@ -18,23 +18,23 @@ while running:
     computer = random.choice(options)
 
     while player not in options:
-        player = input("Enter a choice (rock, paper, scissors): ")
+        player = input("Välj sten sax eller påse:): ")
 
     print(f"Player: {player}")
     print(f"Computer: {computer}")
 
     if player == computer:
-        print("It's a tie!")
-    elif player == "rock" and computer == "scissors":
-        print("You win!")
-    elif player == "paper" and computer == "rock":
-        print("You win!")
-    elif player == "scissors" and computer == "paper":
-        print("You win!")
+        print("Det blev oavgjort!")
+    elif player == "sten" and computer == "sax":
+        print("Du winner!")
+    elif player == "papper" and computer == "sten":
+        print("Du winner!")
+    elif player == "sax" and computer == "påse":
+        print("Du winner!")
     else:
-        print("You lose!")
+        print("Du förlorade!")
 
-    if not input("Play again? (y/n): ").lower() == "y":
+    if not input("Vill du spela igen? (j/n): ").lower() == "j":
         running = False
 
 print("Thanks for playing!")
