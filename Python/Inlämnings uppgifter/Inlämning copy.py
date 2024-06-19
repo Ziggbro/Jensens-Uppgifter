7c3ed29a6c7cf70f95be65f9d73b39749c8b15eb
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-19 10:34:59 +0200

    Update Inlämning copy.py

--- Python/Inlämnings uppgifter/Inlämning copy.py
+++ Python/Inlämnings uppgifter/Inlämning copy.py
@@ -20,38 +20,41 @@ val = ("sten","sax","påse")
 #print welcome messege
 print("Let's play a game")
 #print("Please pick wich one.")
-#wichgame = int(print("1:   sten saxs påse \n 2: Tärning \n 3: Something else"))
+wichgame = input("1:    sten saxs påse \n 2:    Tärning \n 3:   Something elsen\n")
 #hopefully invokes first choice to start another function
-"""""
-def gameStart():
-    if input("1"):
-        print("du har valt sten sax påse")
-        tresome()
-    else:
-        print("game machine broken, pick another")
-"""""  
+
+
+if wichgame("1"):
+    print("du har valt sten sax påse")
+    tresome()
+else:
+    print("game machine broken, pick another")
+  
 #sten sax påse spel typ
-def tresome():
-    while happiness:
-        spelare = None
-        dator = random.choice(val)
+#def tresome():
+while happiness:
+    spelare = None
+    dator = random.choice(val)
 
-        while spelare not in val:
-            spelare = input("välj sten sax eller påse")
-        else :
-            print("välj ett riktigt val")
+    while spelare not in val:
+        spelare = input("välj sten sax eller påse\n")
+    else :
+        print("välj ett riktigt val\n")
 
-        print("spelare valde :", spelare)
-        print("Datorn valde:", dator)
-        if spelare == dator:
-            print("Ovavgjort")
-        elif spelare == "sax" and dator == "påse":
-            print("du vinner")
-        elif spelare == "påse" and dator == "sten":
-            print("du vinner")
-        elif spelare == "sten" and dator == "sax":
-            print("du vinner")
-        else:
-            print("Du förlorade")
+    print("spelare valde :", spelare)
+    print("Datorn valde:", dator)
+    if spelare == dator:
+        print("Ovavgjort")
+    elif spelare == "sax" and dator == "påse":
+        print("du vinner")
+    elif spelare == "påse" and dator == "sten":
+        print("du vinner")
+    elif spelare == "sten" and dator == "sax":
+        print("du vinner")
+    else:
+        print("Du förlorade")
+        
+    if not input("Vill du spela igen? (j/n): ").lower() == "j":
+        happiness = False
     
-    
+    print("Trevligt spelande")
