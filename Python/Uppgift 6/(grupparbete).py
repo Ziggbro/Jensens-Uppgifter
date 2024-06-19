9963cfcbea72e7a7608f9fe73580ffcdffffe91b
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-19 13:45:55 +0200

    Create (grupparbete).py

---
+++ Python/Uppgift 6/(grupparbete).py
@@ -0,0 +1,15 @@
+
+
+
+while True:
+    n = int(input("Enter N:\n"))
+    summa = 0
+    counter = 1
+    
+    while counter <= n:
+        summa = summa + counter
+        counter = counter+1
+    
+    print("summan är: ",summa)
+    break
+
