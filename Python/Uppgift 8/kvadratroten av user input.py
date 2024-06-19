9d2f6a9cc0bb813b51b2cc6f28fee065acfc15ce
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-19 11:09:54 +0200

    Update kvadratroten av user input.py

--- Python/Uppgift 8/kvadratroten av user input.py
+++ Python/Uppgift 8/kvadratroten av user input.py
@@ -4,8 +4,8 @@ import math
 
 
 while True:
-    tal = int(input("skrive det tal du vill ha kvadratroten av"))
-    if tal >20:
+    tal = int(input("skriv det tal du vill ha kvadratroten av\n"))
+    if tal>20:
         print("Kvadratroten av ",tal, "är ",math.sqrt(tal))
-    else :
+    else:
         print("kvadraten är:", math.pow(tal,2))
