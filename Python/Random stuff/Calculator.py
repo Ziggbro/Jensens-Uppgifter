4b507f5891e87e32233b4b566bffb0b18d49df97
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-06-24 11:51:15 +0200

    Update Calculator.py

--- Python/Random stuff/Calculator.py
+++ Python/Random stuff/Calculator.py
@@ -5,3 +5,15 @@ operator = input("enter modifer")
 num1 = float(input("enter the first number"))
 num2 = float(input("enter the seccond number"))
 
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result num1 * num2
+elif operator == "/":
+    result num1 / num2
+else:
+    print("error")
+
+print(result)
