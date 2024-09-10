f186280cdbf1ade88adcff1a513c8fb0a88c564e
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-10 13:58:51 +0200

    fix

--- Python/Python 2/class.py
+++ Python/Python 2/class.py
@@ -1,14 +1,29 @@
-class Book:
-    def __init__(self, title, author, year):
+# class Book:
+#     def __init__(self, title, author, year):
 
-        self.title = title
-        self.author = author
-        self.year = year
+#         self.title = title
+#         self.author = author
+#         self.year = year
         
-    def describe(self):
-        return f"{self.title}, by {self.author}, published in {self.year}"
+#     def describe(self):
+#         return f"{self.title}, by {self.author}, published in {self.year}"
     
 
-book1 = Book("The Stockholm City", "Pierre G", "2018")
-print(book1.title)
-print(book1.describe())
+# book1 = Book("The Stockholm City", "Pierre G", "2018")
+# print(book1.title)
+# print(book1.describe())
+
+
+
+class Customer():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def hello(self):
+        return f"hej och välkommen {self.name}, {self.age}"
+
+
+costumer1 = Customer("alice", "400")
+
+print(costumer1.hello())
