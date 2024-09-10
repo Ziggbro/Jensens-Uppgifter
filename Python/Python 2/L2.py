df247f96450fe930c99713b38c1dc4facb4e2e64
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-10 13:35:41 +0200

    broken code idk

---
+++ Python/Python 2/L2.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+# fil = open("d:\python.txt", "a")
+# fil.write("I like Python")
+# fil.close()
+
+# # Öppna och läs filen efter tillägget:
+# fil = open("d:\python.txt", "r")
+# print(fil.read())
+# fil.close()
+
+fil = open("c:\python.txt", "a")
+fil.write(" I like webdev")
+fil.close()
+
+fil = open("c:\python.txt ","r")
+print(fil.read())
+fil.close()
+
