f02665a00081b05b665d4940b421bac921d4f880
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-10 14:33:47 +0200

    Update class.py

--- Python/Python 2/class.py
+++ Python/Python 2/class.py
@@ -15,15 +15,21 @@
 
 
 
-class Customer():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Customer():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def hello(self):
-        return f"hej och välkommen {self.name}, {self.age}"
+#     def hello(self):
+#         return f"hej och välkommen {self.name}, {self.age}"
 
 
-costumer1 = Customer("alice", "400")
+# costumer1 = Customer("alice", "400")
 
-print(costumer1.hello())
+# print(costumer1.hello())
+
+
+
+
+
+
