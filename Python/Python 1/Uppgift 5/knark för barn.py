0574f8325903f6af252d81371d2b326108eba97a
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-11 13:03:50 +0200

    fix

--- Python/Python 1/Uppgift 5/knark för barn.py
+++ Python/Python 1/Uppgift 5/knark för barn.py
@@ -2,13 +2,14 @@
 print("Här får du reda på hur stark dos Patienten behöver.")
 while True:
     pick = input("Är patienten ett barn?").lower()
-    if pick == 'ja' or 'j':
+    if pick == "ja" or pick == "j":
         print("Patienten behöver bara 500mg per dag.")
         break
-    elif pick == 'nej'or 'n':
+    elif pick == "n" or pick == "nej":
         print("Patienten behöver 700mg per dag.")
         break
     else:
         print("Vi måste veta om du ska få rätt mängd medicine")
+        
 
  
