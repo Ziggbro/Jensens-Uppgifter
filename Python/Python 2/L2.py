623ec37af3f0288a42df7629d97a43f60971818e
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-11 12:00:49 +0200

    fixed the read path

--- Python/Python 2/L2.py
+++ Python/Python 2/L2.py
@@ -13,11 +13,11 @@
 # print(fil.read())
 # fil.close()
 
-fil = open("..\Python\Python 2\python.txt", "a")
+fil = open("Python\Python 2\python.txt", "a")
 fil.write(" I like webdev")
 fil.close()
 
-fil = open("c:\python.txt ","r")
+fil = open("python.txt ","r")
 print(fil.read())
 fil.close()
 
