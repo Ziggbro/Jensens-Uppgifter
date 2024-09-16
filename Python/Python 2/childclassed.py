32c0177bfd4836b449716a2ccd99ba2163662ac3
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-16 13:35:20 +0200

    copy paste

---
+++ Python/Python 2/childclassed.py
@@ -0,0 +1,21 @@
+
+
+
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+class Student(Person):
+    def __init__(self, fname, lname, ålder):
+        super().__init__(fname, lname)
+        self.ålder = ålder
+
+    def printname(self):
+        print(f"Namn: {self.firstname} {self.lastname}, \n Ålder: {self.ålder}")
+
+
+x = Student("Mike", "Olsen", 35)
+
+x.printname()
