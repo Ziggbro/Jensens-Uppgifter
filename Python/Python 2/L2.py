58fddee1521c334bbf738318bc812ad4a549e9b9
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2024-09-16 13:25:41 +0200

    .

--- Python/Python 2/L2.py
+++ Python/Python 2/L2.py
@@ -28,15 +28,40 @@ import os
 # fil.write("\n name3")
 # fil.close()
 
-fil = open("Python\Python 2\listname.txt","r")
-print(fil.read())
+# fil = open("Python\Python 2\listname.txt","r")
+# print(fil.read())
 
 
-with open("Python\Python 2\listname.txt") as file:
-    for line in file:
-        line = line.strip()
-        print("hej, ", line, "trevlig helg")
+# with open("Python\Python 2\listname.txt") as file:
+#     for line in file:
+#         line = line.strip()
+#         print("hej, ", line, "trevlig helg")
 
-fil.close()
+# fil.close()
+
+import os
+import os.path
+deltagare = ['Samer', 'Olivia', 'Ove','Sara']
+
+os.path.isfile("Python\Python 2\my_friends\exaple.txt")
+
+# if os.path.exists('Python\Python 2\my_friends'):
+#     os.remove("Python\Python 2\my_friends\exaple.txt")
+#     os.rmdir("Python\Pyton 2\my_friends")
+#     print("filen har tagits ort")
+# else:
+#     print("gay")
+
+if not os.path.exists('Python\Pyton 2\my_friends'):
+    os.makedirs('Python\Python 2\my_friends')
+
+
+with open('Python\Python 2\my_friends\exaple.txt', 'w',) as fil:
+    for y in deltagare:
+        fil.write('Hej '+ y + '!\n')
+        fil.close()
+    
+
+
 
 
