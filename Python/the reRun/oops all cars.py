e23e708c91216ec5a6165605524e070d6c4b743e
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2025-06-11 09:27:40 +0200

    classic oop sample with books

---
+++ Python/the reRun/oops all cars.py
@@ -0,0 +1,11 @@
+
+
+
+class Book:
+    def __init__ (self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+def describe (self):
+    return f"{self.title} by {self.author} in ({self.year})"
