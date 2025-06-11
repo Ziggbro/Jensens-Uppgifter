85306c2b13ad3cc85e0c008c1176cd4a025d6d40
Ziggbro <90351730+Ziggbro@users.noreply.github.com>
2025-06-11 11:47:32 +0200

    removed books and added cars that drive

--- Python/the reRun/oops all cars.py
+++ Python/the reRun/oops all cars.py
@@ -1,11 +1,35 @@
 
+class Car:
 
+    def __init__(self, brand, model, year, speed = 0.0):
 
-class Book:
-    def __init__ (self, title, author, year):
-        self.title = title
-        self.author = author
+        self.brand = brand
+        self.model = model
         self.year = year
+        self.speed = 0.0
 
-def describe (self):
-    return f"{self.title} by {self.author} in ({self.year})"
+
+    def drive(self, input_speed):
+        self.speed = input_speed
+
+    def stop(self):
+        self.speed = 0.0
+
+
+
+car1 = Car("Honda", "Civic", 2021)
+car2 = Car("Toyota", "Corolla", 2022)
+
+car1.drive(50)
+print("Bil 1 hastighet:", car1.speed)
+
+car2.drive(70)
+print("Bil 2 hastighet:", car2.speed)
+
+car1.stop()
+print("Bil 1 hastighet efter stop:", car1.speed)
+print("Bil 2 hastighet:", car2.speed)
+
+
+
+
